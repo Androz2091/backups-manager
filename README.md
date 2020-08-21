8f2d8221accb53001818322167ff625f87c99493
Androz2091 <androz2091@gmail.com>
2020-08-21 08:08:34 +0200

    :tada: First commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Backups Manager
+
+⚡ Gère vos fichiers de sauvegarde de manière intelligente.
+
+## Fonctionnalités
+
+Scanne votre dossier de fichiers de sauvegarde (sous le format `backupname-2020-08-01-1200.sql` par exemple) et applique les règles suivantes :
+
+* Tous les fichiers des dernières 24 heures sont gardés.
+* Un fichier par jour est gardé, pendant une semaine (celui créé à minuit).
+* Un fichier par semaine est gardé, pendant un mois (celui créé le dimanche).
+* Un fichier par mois est gardé, pendant un an (celui créé le premier du mois).
+
+Les autres fichiers sont supprimés automatiquement.
+
+## CronJob
+
+CronJob pour exécuter automatiquement le script à une heure du matin:
+
+```sh
+0 1 * * * /usr/bin/ruby backup_manager.rb
+```
---
+++ backup_manager.rb
@@ -0,0 +1,36 @@
+require 'date'
+
+directory_path = ARGV[0]
+
+Dir.foreach(directory_path) do |filename|
+
+    # ignore system files
+    next if filename == '.' or filename == '..'
+
+    # retrieve date from file name
+    name, year, month, day, hours_and_minutes = filename.split('-')
+    hours_and_minutes = hours_and_minutes.split('.')[0]
+    hours = hours_and_minutes[0..-3]
+    minutes = hours_and_minutes[2..-1]
+    time = Time.new(year, month, day, hours, minutes, 0)
+
+    # backups created during the last 24 hours should be kept
+    very_recent_param = (time.to_i > (Time.now.to_i - 86400))
+
+    # backups created during the last week and at midnight should be kept
+    one_per_day_param = time.hour === 0 && (time.to_i > (Time.now.to_i - 604800))
+
+    # backups created during the last month and a sunday should be kept
+    one_per_week_param = time.sunday? && (time.to_i > (Time.now.to_i - 2678400))
+
+    # backups created during the last year and the first of the month should ke kept
+    one_per_month_param = time.day === 1 && (time.to_i > (Time.now.to_i - 31622400))
+    
+    # backups the file
+    unless very_recent_param || one_per_day_param || one_per_week_param || one_per_month_param
+        File.delete "#{directory_path}#{filename}"
+        print "#{filename} deleted.\n"
+    else
+        print "#{filename} kept.\n"
+    end
+end
